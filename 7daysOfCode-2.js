425dd55c2363dd3ab195afee9127ff51e0d4d92d
RayaneSousa <87087454+RayaneSousa@users.noreply.github.com>
2022-04-02 00:21:28 -0300

    Add files via upload

---
+++ 7daysOfCode-2.js
@@ -0,0 +1,15 @@
+var nome = prompt("Qual o seu nome?");
+
+var idade = prompt("Quantos anos você tem?");
+
+var linguagem= prompt("Qual a linguagem de programação você está estudando?");
+
+alert("Olá "+nome+ " ,você tem " +idade+ " anos e já está aprendendo " +linguagem+ "!");
+
+var resposta = prompt("Você gosta de estudar "+linguagem+"? Responda com o número 1 para SIM ou 2 para NÃO.");
+
+if(resposta == "1"){
+    alert("Muito bom! Continue estudando e você terá muito sucesso.");
+}else if(resposta == "2"){
+    alert("Ahh que pena...Já tentou aprender outras linguagens?");
+}
